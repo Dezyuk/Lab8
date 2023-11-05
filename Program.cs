ae6b66b191311cff83dcfc21911df3842f13a7d3
Dezyuk <romanyuk.groz@gmail.com>
2023-11-05 13:56:36 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using Lab8;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PartsWarehouse part1 = new PartsWarehouse("Drill", "fds51s1", 50.584, 150);
+        PartsWarehouse part2 = new PartsWarehouse("Pump", "5d8sf11c", 1000.00, 58);
+        PartsWarehouse part3 = new PartsWarehouse("Valve", "9df2q3f", 405.35, 7);
+        PartsWarehouse part4 = new PartsWarehouse("Sealer", "a1f4y2n", 18.99, 200);
+
+
+        Storehouse store = new Storehouse();
+        store.AddParts(part1);
+        store.AddParts(part2);
+        store.AddParts(part3);
+        store.AddParts(part4);
+
+        Console.WriteLine(store);
+        Console.WriteLine("--------------------------------------------------");
+        store.RemovePart(part1);
+
+        Console.WriteLine(store);
+        string json;
+
+        string output = JsonSerializer.Serialize(store);
+        
+        
+
+        using (FileStream fileStream = new FileStream("Parts.json", FileMode.OpenOrCreate))
+        {
+            using (StreamWriter writer = new StreamWriter(fileStream))
+            {
+                writer.Write(output);
+            }
+        }
+
+
+        
+//        Stream stream = File.Open("Parts.bin", FileMode.OpenOrCreate);
+//#pragma warning disable SYSLIB0011
+//        BinaryFormatter bf = new BinaryFormatter();
+//        bf.Serialize(stream, store);
+//#pragma warning restore SYSLIB0011
+//        stream.Close();
+
+
+//        using (var bin = new FileStream("Parts.bin", FileMode.OpenOrCreate))
+//        {
+//#pragma warning disable SYSLIB0011
+//            BinaryFormatter formatter = new BinaryFormatter();
+//            formatter.Serialize(bin, store);
+//#pragma warning restore SYSLIB0011
+//        }
+
+
+
+
+        using (FileStream fileStream = new FileStream("Parts.json", FileMode.Open)) { 
+        using (StreamReader  reader = new StreamReader(fileStream))
+            {
+                json = reader.ReadToEnd();
+            }
+        }
+        Storehouse store2 = JsonSerializer.Deserialize<Storehouse>(json);
+        Console.WriteLine("New Store");
+        Console.WriteLine(store2);
+    }
+}
---
+++ Storehouse.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Lab8
+{
+    /// <summary>
+    /// Storehouse — класс для управления запасными частями в складе.
+    /// </summary>
+    public class Storehouse
+    {
+        private List<PartsWarehouse> _warehouseList;
+        //List<PartsWarehouse> warehouseList = new List<PartsWarehouse>();
+
+        public Storehouse(List<PartsWarehouse> list) 
+        {
+            WarehouseList = list;
+        }
+        public Storehouse()
+        {
+            WarehouseList = new List<PartsWarehouse>();
+        }
+        public List<PartsWarehouse> WarehouseList 
+        {
+            get => _warehouseList; 
+            set => _warehouseList = (value is null ? new List<PartsWarehouse>() : value); 
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// Добавление новой запчасти в List.
+        /// </summary>
+        /// <param name="newPart">Новая деталь которую необходимо добавить, проверяется есть ли уже данный объект в List через проверку Id.</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void AddParts(PartsWarehouse newPart)
+        {
+            if (newPart is not null)
+            {
+                foreach (PartsWarehouse part in WarehouseList)
+                {
+                    if (part.Id.Equals(newPart.Id))
+                    {
+                        throw new ArgumentOutOfRangeException("This part is already in Storage!");
+                    }
+                }
+                WarehouseList.Add(newPart);
+            }
+            else
+            {
+                Console.WriteLine(newPart);
+                throw new ArgumentOutOfRangeException("This part is already in Storage!");
+            }
+        }
+
+        /// <summary>
+        /// Удаление запчасти из List.
+        /// </summary>
+        /// <param name="partToRemove">Деталь которую необходимо удалить.</param>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public void RemovePart(PartsWarehouse partToRemove)
+        {
+            if (partToRemove is not null && WarehouseList.Contains(partToRemove))
+            {
+                WarehouseList.Remove(partToRemove);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("This part does not exist!");
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (PartsWarehouse part in WarehouseList)
+            {
+                str.Append(part + "\n");
+            }
+            return str.ToString();
+        }
+    }
+}
